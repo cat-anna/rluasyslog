847ace07b94415f7b6e3c3700582282b927f7906
Pawel Grabas <pgrabas@gmail.com>
2018-03-16 08:25:46 +0100

    update espeasy parser

--- parser-espeasy.lua
+++ parser-espeasy.lua
@@ -10,6 +10,8 @@ end
 function ParserEspEasy:Init(args)
 end
 
+--<7>ESP Unit: 2 : WD   : Uptime 57859 ConnectFailures 0 FreeMem 22200
+
 local ESPEasyRegExp = [==[%<(%d+)%>ESP%s+Unit:%s+(%d+)%s*:%s*(%w+)%s*:%s*(.+)]==]
 function ParserEspEasy:TryParse(data)
     local pri, unit, group, message = data.data:match(ESPEasyRegExp)
